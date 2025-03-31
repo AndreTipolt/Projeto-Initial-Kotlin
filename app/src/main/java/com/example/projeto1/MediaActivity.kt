4068953208748a2ac8f6ab206fbeaa746a529c79
André Tipolt <andretipoltlopes@gmail.com>
2025-03-31 19:58:08 -0300

    Passando as string da tela de media

--- app/src/main/java/com/example/projeto1/MediaActivity.kt
+++ app/src/main/java/com/example/projeto1/MediaActivity.kt
@@ -1,6 +1,7 @@
 package com.example.projeto1
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,15 @@ class MediaActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val btnCalcular: Button = findViewById(R.id.btnCalcular)
+
+        btnCalcular.setOnClickListener {
+            calcularMedia()
+        }
+    }
+
+    fun calcularMedia() {
+
     }
 }
