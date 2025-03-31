9abdea39b9c457990f32aee84f4097c882fa3ded
André Tipolt <andretipoltlopes@gmail.com>
2025-03-31 20:28:13 -0300

    Criando lógica para calcular média e fazer a validação de valores

--- app/src/main/java/com/example/projeto1/MediaActivity.kt
+++ app/src/main/java/com/example/projeto1/MediaActivity.kt
@@ -2,10 +2,14 @@ package com.example.projeto1
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MediaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,5 +31,44 @@ class MediaActivity : AppCompatActivity() {
 
     fun calcularMedia() {
 
+        val txtResultado: TextView = findViewById(R.id.txtResultado);
+
+        txtResultado.text = null;
+
+        val txtNota1 = findViewById<TextInputEditText>(R.id.txtNota1);
+        val txtNota2  = findViewById<TextInputEditText>(R.id.txtNota2);
+        val txtNota3 = findViewById<TextInputEditText>(R.id.txtNota3);
+
+        val nota1 = txtNota1.text?.toString()?.toFloatOrNull()
+        val nota2 = txtNota2.text?.toString()?.toFloatOrNull()
+        val nota3 = txtNota3.text?.toString()?.toFloatOrNull()
+
+        if(nota1 == null || nota2 == null || nota3 == null) {
+
+            Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(nota1 > 10 || nota1 < 0) {
+            Toast.makeText(this, "Nota 1 Inválida", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(nota2 > 10 || nota2 < 0) {
+            Toast.makeText(this, "Nota 2 Inválida", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(nota3 > 10 || nota3 < 0) {
+            Toast.makeText(this, "Nota 3 Inválida", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        val list = listOf(nota1, nota2, nota3);
+
+        val resultado: Float = list.sum() / list.size;
+
+        if(resultado >= 6) txtResultado.text = "Você está Aprovado. Parabéns!"
+         else txtResultado.text = "Você está reprovado."
     }
 }
